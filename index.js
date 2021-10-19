2a13a070de23c251317f63f2a2de722b795eb6c4
Austin Durham <austindurham@Austins-MBP.lan>
2021-10-18 23:25:51 -0700

    Request for spStart is working

--- index.js
+++ index.js
@@ -3,54 +3,17 @@
 // https://www.n2yo.com/passes/?s=33591  Post Request
 
 
-const axios = require('axios');
 const express = require("express");
+const connect = require("./savedFiles");
+const {getRequest} = require("./savedFiles");
 const router = express.Router();
-// const {response} = require("express");
+const app = express();
 
-axios.get( 'https://www.n2yo.com/passes/?s=33591')
-    .then((response) => {
-      var rawData = response.data;
+app.get('/test', function(req, res){
 
+  getRequest().then(function(result) {
+    res.send(result);
+  })
+});
 
-      // console.log(rawData)
-
-      // let splitOne = rawData.split("<SCRIPT");
-      let split = rawData.split('"Dec"];')[1].split('function SatellitePosition')[0];
-
-
-
-      // for (let i = 0; i < lines.length; i++) {
-      //   if ( /passes/.test(lines) ) {
-      //     console.log()
-      //   }
-      //
-      // }
-      // let splitTwo = splitOne[1].split("<SCRIPT>")
-      // return(splitTwo[0])
-
-      // var lines_with_me = split.split("\n").filter(function(a){return /\bpasses\b/.test(a); });
-      // console.log(lines_with_me.length);
-
-
-      var spStart = split.split("\n").filter(function(a){return /\bspStart = \b/.test(a); });
-      console.log(spStart);
-
-      var spMax = split.split("\n").filter(function(a){return /\bspMax = \b/.test(a); });
-      console.log(spMax);
-
-      var spEnd = split.split("\n").filter(function(a){return /\bspEnd = \b/.test(a); });
-      console.log(spEnd);
-
-    });
-
-
-// const express = require("express");
-// const router = express.Router();
-// const app = express();
-//
-// app.get('/test', function(req, res){
-//       res.send('hello world');
-// });
-//
-// app.listen(3000);
+app.listen(3000);
